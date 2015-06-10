4a33fc5fb5e153d86d6df37fdeb0b9a0eff9ab1c
Adam Strickland <dev+adamstrickland@wellmatchhealth.com>
2015-06-10 12:26:31 -0500

    working 2-way ws

--- web_client/server.js
+++ web_client/server.js
@@ -14,12 +14,8 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var path = require('path');
-// var io = require('socket.io')(server);
 
 server.listen(3000);
-
-// console.log("using API at "+process.env.API_HOST);
-
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
@@ -32,74 +28,3 @@ app.get('/build/*', function (req, res) {
 
 // app.get('/', function (req, res) { res.render('index', { api_host: process.env.API_HOST }); });
 app.get('/', function (req, res) { res.sendFile(__dirname + '/public/index.html'); });
-
-// io.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
-
-// var fs = require('fs');
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var app = express();
-// var server = require('http').Server(app);
-// var io = require('socket.io')(server);
-
-// // var socket = require('socket.io');
-
-// app.set('port', (process.env.PORT || 3000));
-
-// app.use('/', express.static(path.join(__dirname, 'public')));
-// app.use('/build', express.static(path.join(__dirname, 'build')));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-
-// // app.use(req, res, next, function() {
-// //   // res.header("Access-Control-Allow-Origin", "*");
-// //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-// //   res.setHeader('Access-Control-Allow-Origin', 'https://quivering-kitten-9504.dev.wellmatchhealth.com');
-// //   res.setHeader('Access-Control-Allow-Methods', 'GET');
-// //   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-// //   res.setHeader('Access-Control-Allow-Credentials', true);
-// //   next();
-// // });
-
-// // app.get('/socket.io', function(req, res){
-// // });
-
-// app.get('/events', function(req, res){
-// });
-
-// app.get('/comments.json', function(req, res) {
-//   fs.readFile('comments.json', function(err, data) {
-//     res.setHeader('Content-Type', 'application/json');
-//     res.send(data);
-//   });
-// });
-
-// app.post('/comments.json', function(req, res) {
-//   fs.readFile('comments.json', function(err, data) {
-//     var comments = JSON.parse(data);
-//     comments.push(req.body);
-//     fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
-//       res.setHeader('Content-Type', 'application/json');
-//       res.setHeader('Cache-Control', 'no-cache');
-//       res.send(JSON.stringify(comments));
-//     });
-//   });
-// });
-
-// app.listen(app.get('port'), function() {
-//   console.log('Server started: http://localhost:' + app.get('port') + '/');
-// });
-
-// server.listen(4001);
-
-// io.sockets.on('connection', function (socket) {
-//   // stream.on('tweet', function(tweet) {
-//   //   socket.emit('info', { tweet: tweet});
-//   // });
-// });
--- web_client/src/app.js
+++ web_client/src/app.js
@@ -1,47 +1,47 @@
-// var host = 'ws://node-api.foo.com:4000'
-var host = "wss://undying-funereal-skull.admin.wellmatchhealth.com"
+var host = 'ws://node-api.foo.com:4000'
+// var host = "wss://undying-funereal-skull.admin.wellmatchhealth.com"
 var socket = io.connect(host)
+
 socket.on('news', function (data) {
   console.log(data);
   socket.emit('my other event', { my: 'data' });
 });
 
-// import {Socket} from "./phoenix.js"
-// "use strict";
-// var Socket = require('socket.io-client');
-// var Socket = require('socket.io');
+socket.on('chat', (payload) => {
+  console.log(payload);
+  $('#messages').append(`<br/>[${Date()}] ${payload.message}`)
+});
 
-// var host = "https://undying-funereal-skull.admin.wellmatchhealth.com"
-// var host = "ws://localhost:3000"
-// var url = host + "/events"
+var chatInput = $('#chat-input');
+chatInput.on('keypress', (e) => {
+  if(e.keyCode === 13){
+    socket.emit('chat', { message: chatInput.val() });
+    chatInput.val('');
+  }
+});
 
-// var socket = io(host);
-// // var socket = require('socket.io-client')(host, {secure: true});
-// var Socket = require('./phoenix.js')
-// (function () {
+/* THE PHOENIX IMPL */
+// let socket = new Socket(host)
+// var socket = Socket.io(host, {secure: true});
+// socket.connect()
+// socket.connect(host, {secure: true});
 
-  // let socket = new Socket(host)
-  // var socket = Socket.io(host, {secure: true});
-  // socket.connect()
-  // socket.connect(host, {secure: true});
+// let chatInput = $("#chat-input")
+// let messagesContainer = $("#messages")
 
-  // let chatInput = $("#chat-input")
-  // let messagesContainer = $("#messages")
+// let chan = socket.chan("events:search:1234", {})
 
-  // let chan = socket.chan("events:search:1234", {})
+// chatInput.on("keypress", event => {
+//   if(event.keyCode === 13) {
+//     chan.push("new_msg", {body: chatInput.val()})
+//     chatInput.val("")
+//   }
+// })
 
-  // chatInput.on("keypress", event => {
-  //   if(event.keyCode === 13) {
-  //     chan.push("new_msg", {body: chatInput.val()})
-  //     chatInput.val("")
-  //   }
-  // })
+// chan.on("new_msg", payload => {
+//   messagesContainer.append(`<br/>[${Date()}] ${payload.body}`)
+// })
 
-  // chan.on("new_msg", payload => {
-  //   messagesContainer.append(`<br/>[${Date()}] ${payload.body}`)
-  // })
-
-  // chan.join().receive("ok", chan => {
-  //   console.log("Welcome to Phoenix Chat!")
-  // })
-// })();
+// chan.join().receive("ok", chan => {
+//   console.log("Welcome to Phoenix Chat!")
+// })
